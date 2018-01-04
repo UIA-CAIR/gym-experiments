7545ad11c603c6fce73cfcb13b679e6b2d830cb5
Per-Arne Andersen <per@sysx.no>
2018-01-04 18:10:54 +0100

    Added template for datagen

--- component/datagen/training_data_generator.py
+++
@@ -1,9 +0,0 @@
-import gym
-import gym_deeplinewars
-#import gym_maze
-#import gym_deeprts
-#import gym_flashrl
-
-
-
-
---
+++ components/datagen/main.py
@@ -0,0 +1,7 @@
+import deeplinewars
+import deepmaze
+import deeprts
+import flashrl
+import flappybird
+
+print("Done!")
